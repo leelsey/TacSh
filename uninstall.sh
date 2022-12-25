db6b4618cdc180e2bef1ddcc0a747a7d3be1c312
Leslie Elsey <108100202+leelsey@users.noreply.github.com>
2022-12-25 23:39:21 +0900

    Create unintall.sh

---
+++ uninstall.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env zsh
+
+tacshDirPath="$HOME/.config/tacsh"
+# tacshFilePath="$tacDirPath/tac.sh"
+
+echo '- Remove Alias4sh ... \c'
+if [ -n "`$SHELL -c 'echo $ZSH_VERSION'`" ]; then
+    profileName=".zprofile"
+elif [ -n "`$SHELL -c 'echo $BASH_VERSION'`" ]; then
+    profileName=".bash_profile"
+else 
+    echo "NO\n • Your shell is not supported!"
+    exit 1
+fi
+rm -rf $tacshDirPath
+echo "OK \n • Finish to remove TacSh! \n • Please remove manually in ~/$profileName"
