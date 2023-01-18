405c022c8b1626f7a42af0577666f4e57bac77ac
Leslie Elsey <108100202+leelsey@users.noreply.github.com>
2023-01-18 20:52:36 +0900

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,15 +1,22 @@
 #!/usr/bin/env zsh
-
 tacshDirPath="$HOME/.config/tacsh"
-
 echo '- Remove Alias4sh ... \c'
-if [ -n "`$SHELL -c 'echo $ZSH_VERSION'`" ]; then
+if [ "$(uname)" = "Darwin" ]; then
     profileName=".zprofile"
-elif [ -n "`$SHELL -c 'echo $BASH_VERSION'`" ]; then
-    profileName=".bash_profile"
-else 
-    echo "NO\n • Your shell is not supported!"
+elif [ "$(uname)" = "Linux" ]; then
+    if [ -n "`$SHELL -c 'echo $ZSH_VERSION'`" ]; then
+        profileName=".zshrc"
+    elif [ -n "`$SHELL -c 'echo $BASH_VERSION'`" ]; then
+        profileName=".bashrc"
+    fi
+else
+    echo "NO\n • Your OS is not supported!"
     exit 1
 fi
-rm -rf $tacshDirPath
+if [ -d "$tacshDirPath" ]; then
+    rm -rf $tacshDirPath
+else
+    echo -e "NO \n • Someting wrong. Please check persmission or file path."
+    exit 1
+fi
 echo "OK \n • Finish to remove TacSh! \n • Please remove manually in ~/$profileName"
