7a77f2151268585108b88469b276635ad0d818d8
Leslie Elsey <108100202+leelsey@users.noreply.github.com>
2023-01-28 16:52:29 +0900

    Refactoring uninstaller

--- uninstall.sh
+++ uninstall.sh
@@ -1,57 +1,52 @@
 #!/usr/bin/env bash
 tacshDirPath="$HOME/.config/tacsh"
-confirmUninstall() {
-    read -p "Are you sure to remove TacSh? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
-}
-if [ -d "$tacshDirPath" ]; then
-    if [ "$(uname)" = "Darwin" ]; then
-        echo -e '- Uninstalling TacSh ... \c'
-        tacshFileName="tac.sh"
-        tacshFilePath="$tacshDirPath/$tacshFileName"
-        profileName=".zprofile"
-        if [ -f "$tacshFilePath" ]; then
-            confirmUninstall
-            rm -f $tacshFilePath
+echo -e "TacSh uninstaller"
+uninstall() {
+    if [ -f $2 ]; then
+        read -p "Are you sure to remove TacSh? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
+        echo -e "- Uninstalling TacSh$4 ... \c"
+        if [ $1 == "1" ]; then
             rm -rf $tacshDirPath
+        elif [ $1 == "2" ]; then
+            rm -f $2
         else
-            echo -e " • TacSh is not installed."
             exit 1
-        fi
-    elif [ "$(uname)" = "Linux" ]; then
-        if [ -n "`$SHELL -c 'echo $ZSH_VERSION'`" ]; then
-            tacshFileName="tac.zsh"
-            tacshFilePath="$tacshDirPath/$tacshFileName"
-            profileName=".zshrc"
-        elif [ -n "`$SHELL -c 'echo $BASH_VERSION'`" ]; then
-            tacshFileName="tac.bash"
-            tacshFilePath="$tacshDirPath/$tacshFileName"
-            profileName=".bashrc"
-        else
-            echo -e " • Your shell is not supported."
-            exit 1
-        fi
-        if [ -f "$tacshFilePath" ]; then
-            confirmUninstall
-        else
-            echo -e " • TacSh is not installed."
-            exit 1
-        fi
-        if [ -f "$tacshDirPath/tac.zsh" ] && [ -f "$tacshDirPath/tac.bash" ] ; then
-            read -p "Which one would you like to delete? (1: $tacshFileName, 2. all): " choose && [[ $choose == [12as] ]] || exit 1
-            if [[ "$choose" == "1" || "$choose" == "s" ]]; then
-                echo -e '- Uninstalling TacSh $tacshFileName ... \c'
-                rm -f $tacshFilePath
-            elif [[ "$choose" == "2" || "$choose" == "a" ]]; then
-                echo -e '- Uninstalling TacSh all ... \c'
-                rm -rf $tacshDirPath
-            fi
         fi
     else
-        echo -e " • Your OS is not supported."
+        echo -e " • TacSh is not installed."
         exit 1
     fi
+    echo -e "OK \n • Finish to uninstall TacSh$4! \n • Please remove manually in ~/$3"
+}
+if [ "$(uname)" = "Darwin" ]; then
+    tacshFileName="tac.sh"
+    tacshFilePath="$tacshDirPath/$tacshFileName"
+    profileName=".zprofile"
+    uninstall 1 $tacshFilePath $profileName ""
+elif [ "$(uname)" = "Linux" ]; then
+    if [ -n "`$SHELL -c 'echo $ZSH_VERSION'`" ]; then
+        tacshFileName="tac.zsh"
+        tacshFilePath="$tacshDirPath/$tacshFileName"
+        profileName=".zshrc"
+    elif [ -n "`$SHELL -c 'echo $BASH_VERSION'`" ]; then
+        tacshFileName="tac.bash"
+        tacshFilePath="$tacshDirPath/$tacshFileName"
+        profileName=".bashrc"
+    else
+        echo -e " • Your shell is not supported."
+        exit 1
+    fi
+    if [ -f "$tacshDirPath/tac.zsh" ] && [ -f "$tacshDirPath/tac.bash" ] ; then
+        read -p "Which shell do you want to uninstall? (1: $tacshFileName, 2. all): " choose && [[ $choose == [12as] ]] || exit 1
+        if [[ "$choose" == "1" || "$choose" == "s" ]]; then
+            uninstall 2 $tacshFilePath $profileName " $tacshFileName"
+        elif [[ "$choose" == "2" || "$choose" == "a" ]]; then
+            uninstall 1 $tacshFilePath $profileName " all"
+        fi
+    else
+        uninstall 1 $tacshFilePath $profileName ""
+    fi
 else
-    echo -e "NO \n • TacSh is not installed."
+    echo -e " • Your OS is not supported."
     exit 1
 fi
-echo -e "OK \n • Finish to remove TacSh! \n • Please remove manually in ~/$profileName"
