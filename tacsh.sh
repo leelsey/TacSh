626c7ae4f0804257a23f7a8895899d7582b640cb
Leslie Elsey <108100202+leelsey@users.noreply.github.com>
2023-02-22 23:41:01 +0900

    Refactoring and add each linux command

--- tacsh.sh
+++ tacsh.sh
@@ -62,14 +62,78 @@ funcGen() {
     funcAddFile "vilogin () { vi \"\$HOME/.$shLin\" ; }"
     funcAddFile "vilogout () { vi \"\$HOME/.$shLout\" ; }"
     funcAddFile "whichos () { echo \$(uname) ; }"
+    if [ $keName = "Linux" ]; then
+        funcAddFile "\n# FIREWALL MANAGEMENT TOOL"
+        if [[ $osName = "Ubuntu" ]]; then
+            funcAddFile "#alias firewall='sudo firewall-cmd' # firewall (firewalld)"
+            funcAddFile "alias ufw='sudo ufw'                # ufw (uncomplicated firewall)"
+            funcAddFile "#alias nft='sudo nft'               # nftables (netfilter table)"
+            funcAddFile "alias iptables='sudo iptables'      # iptables (old netfilter)"
+        elif [[ $osName = "Debian" ]] || [[ $osName = "Kali" ]]; then
+            funcAddFile "#alias firewall='sudo firewall-cmd' # firewall (firewalld)"
+            funcAddFile "#alias ufw='sudo ufw'               # ufw (uncomplicated firewall)"
+            funcAddFile "alias nft='sudo nft'                # nftables (netfilter table)"
+            funcAddFile "alias iptables='sudo iptables'      # iptables (old netfilter)"
+        elif [[ $osName = "Fedora" ]] || [[ $osName = "CentOS" ]] || [[ $osName = "RHEL" ]] || [[ $osName = "SUSE" ]]; then
+            funcAddFile "alias firewall='sudo firewall-cmd'  # firewall (firewalld)"
+            funcAddFile "#alias ufw='sudo ufw'               # ufw (uncomplicated firewall)"
+            funcAddFile "#alias nft='sudo nft'               # nftables (netfilter table)"
+            funcAddFile "alias iptables='sudo iptables'      # iptables (old netfilter)"
+        elif [[ $osName = "Arch" ]]; then
+            funcAddFile "#alias firewall='sudo firewall-cmd' # firewall (firewalld)"
+            funcAddFile "#alias ufw='sudo ufw'               # ufw (uncomplicated firewall)"
+            funcAddFile "#alias nft='sudo nft'               # nftables (netfilter table)"
+            funcAddFile "alias iptables='sudo iptables'      # iptables (old netfilter)"
+        else
+            funcAddFile "#alias firewall='sudo firewall-cmd' # firewall (firewalld)"
+            funcAddFile "#alias ufw='sudo ufw'               # ufw (uncomplicated firewall)"
+            funcAddFile "#alias nft='sudo nft'               # nftables (netfilter table)"
+            funcAddFile "#alias iptables='sudo iptables'     # iptables (old netfilter)"
+        fi
+        funcAddFile "\n# PACKAGE MANAGEMENT TOOL"
+        if [[ $osName = "Ubuntu" ]] || [[ $osName = "Debian" ]] || [[ $osName = "Kali" ]]; then
+            funcAddFile "#alias dpkg='sudo dpkg'             # Debian Package Manager for Debian-based"
+            funcAddFile "#alias apt='sudo apt'               # Advanced Package Tool for Debian-based"
+            funcAddFile "#alias apt-get='sudo apt-get'       # Advanced Package Tool (old) for Debian-based"
+        elif [[ $osName = "Fedora" ]] || [[ $osName = "CentOS" ]] || [[ $osName = "RHEL" ]]; then
+            funcAddFile "#alias rpm='sudo rpm'               # Red Hat Package Manager for RHEL-based"
+            funcAddFile "#alias dnf='sudo dnf'               # Dandified YUM for RHEL-based"
+            funcAddFile "#alias yum='sudo yum'               # Yellowdog Updater, Modified (old) for RHEL-based"
+        elif [[ $osName = "SUSE" ]]; then
+            funcAddFile "#alias zypper='sudo zypper'         # Zen / YaST Packages Patches Patterns Products for SUSE-based"
+        elif [[ $osName = "Arch" ]]; then
+            funcAddFile "#alias pacman='sudo pacman'         # Pacman for arch-based family"
+        else
+            funcAddFile "#alias dpkg='sudo dpkg'             # Debian Package Manager for Debian-based"
+            funcAddFile "#alias apt='sudo apt'               # Advanced Package Tool for Debian-based"
+            funcAddFile "#alias apt-get='sudo apt-get'       # Advanced Package Tool (old) for Debian-based"
+            funcAddFile "#alias rpm='sudo rpm'               # Red Hat Package Manager for RHEL-based"
+            funcAddFile "#alias dnf='sudo dnf'               # Dandified YUM for RHEL-based"
+            funcAddFile "#alias yum='sudo yum'               # Yellowdog Updater, Modified (old) for RHEL-based"
+            funcAddFile "#alias zypper='sudo zypper'         # Zen / YaST Packages Patches Patterns Products for SUSE-based"
+            funcAddFile "#alias pacman='sudo pacman'         # Pacman for arch-based family"
+            funcAddFile "#alias emerge='sudo emerge'         # Portage for gentoo-based"
+        fi
+    fi
 
     # About Default Commands Options & Colourising
     funcAddFile "\n# ABOUT DEFAULT OPTIONS WITH COLOURISING"
-    if [ $keName = "Linux" ] || [[ $keName = "MinGW64" ]]; then 
+    if [ $keName = "Linux" ] || [ $keName = "MinGW64" ]; then 
         funcAddFile "rm () { command rm -iv \"\$@\" ; } "
         funcAddFile "mv () { command mv -iv \"\$@\" ; } "
         funcAddFile "cp () { command cp -iv \"\$@\" ; } "
         funcAddFile "ln () { command ln -iv \"\$@\" ; } "
+        funcAddFile "#chmod () { command chmod --preserve-root \"\$@\" ; }"
+        funcAddFile "#chown () { command chown --preserve-root \"\$@\" ; }"
+        funcAddFile "#chgrp () { command chgrp --preserve-root \"\$@\" ; }"
+        funcAddFile "#mkdir () { command mkdir -v \"\$@\" ; } "
+        funcAddFile "#rmdir () { command rmdir -v \"\$@\" ; } "
+        funcAddFile "#vi () { command vim \"\$@\" ; }"
+        funcAddFile "#emcs () { emacs \"\$@\" ; }"
+        funcAddFile "#semcs () { sudo emacs \"\$@\" ; }"
+        funcAddFile "#semacs () { sudo emacs \"\$@\" ; }"
+        funcAddFile "#dfh () { df -h \"\$@\" ; }"
+        funcAddFile "#duh () { du -h \"\$@\" ; }"
     fi
     if [ $keName = "Darwin" ]; then
         funcAddFile "grep () { command grep --color=auto \"\$@\" ; }"
@@ -84,7 +148,7 @@ funcGen() {
         funcAddFile "ls () { command ls -G \"\$@\" ; }"
         funcAddFile "gls () { command gls --color=auto \"\$@\" ; }"
         funcAddFile "dir () { gls -Ao --group-directories-first \"\$@\" ; }"
-    elif [ $keName = "Linux" ] || [[ $keName = "MinGW64" ]]; then
+    elif [ $keName = "Linux" ] || [ $keName = "MinGW64" ]; then
         if [ $shName = "zsh" ]; then
             funcAddFile "grep () { command grep --color=auto \"\$@\" ; }"
             funcAddFile "ls () { command ls --color=auto \"\$@\" ; }"
@@ -106,7 +170,7 @@ funcGen() {
         funcAddFile "l () { ls -C \"\$@\" ; }"
         funcAddFile "ll () { ls -l \"\$@\" ; }"
         funcAddFile "la () { ls -A \"\$@\" ; }"
-    elif [ $keName = "Linux" ] || [[ $keName = "MinGW64" ]]; then
+    elif [ $keName = "Linux" ] || [ $keName = "MinGW64" ]; then
         if [ $shName = "zsh" ]; then
             funcAddFile "l () { ls -C \"\$@\" ; }"
             funcAddFile "ll () { ls -l \"\$@\" ; }"
@@ -175,12 +239,12 @@ funcGen() {
         funcAddFile "if [ -d $dropboxPath ]; then"
         funcAddFile "\tdropbox () { cd $dropboxPath ; ls -A ; }"
         funcAddFile "fi"
-    elif [ $keName = "Linux" ] || [[ $keName = "MinGW64" ]]; then
+    elif [ $keName = "Linux" ] || [ $keName = "MinGW64" ]; then
         funcAddFile "if [ -d $dropboxPath ]; then"
         funcAddFile "\tdropbox () { cd $dropboxPath ; ls -A ; }"
         funcAddFile "fi"
     fi
-    if [ $keName = "Darwin" ] || [[ $keName = "MinGW64" ]]; then
+    if [ $keName = "Darwin" ] || [ $keName = "MinGW64" ]; then
         funcAddFile "ip () { command ipconfig \"\$@\" ;  }" 
     fi
     funcAddFile "dif () { diff \$1 \$2 | bat -l diff ; }"
@@ -240,7 +304,7 @@ funcGen() {
     if [ $keName = "Darwin" ]; then
         funcAddFile "\t elif [[ \$1 == ic ]] || [[ \$1 == icl ]] || [[ \$1 == iCl ]] || [[ \$1 == cl ]] || [[ \$1 == clo ]] || [[ \$1 == Clo ]] ; then"
         funcAddFile "\t\t\tcd '$iCloudPath' ;"
-    elif [ $keName = "Linux" ] || [[ $keName = "MinGW64" ]]; then
+    elif [ $keName = "Linux" ] || [ $keName = "MinGW64" ]; then
         funcAddFile "\t\telif [[ \$1 == vd ]] || [[ \$1 == vid ]] || [[ \$1 == Vid ]]; then"
         funcAddFile "\t\t\tcd ~/Videos ;"
     fi
@@ -271,7 +335,7 @@ funcGen() {
     funcAddFile "\t\t\techo \"p pc, pic: change direcotry to pictures directory\""
     if [ $keName = "Darwin" ]; then
         funcAddFile "\t\t\techo \"p ic, cl, icl, clo: change direcotry to icloud directory\""
-    elif [ $keName = "Linux" ] || [[ $keName = "MinGW64" ]]; then
+    elif [ $keName = "Linux" ] || [ $keName = "MinGW64" ]; then
         funcAddFile "\t\t\techo \"p vd, vid: change direcotry to videos directory\""
     fi
     funcAddFile "\t\t\techo \"p dr, dro, drp: change direcotry to dropbox directory\""
@@ -327,113 +391,41 @@ funcGen() {
         funcAddFile "}"
     fi
 
-    # Alias command part
+    # Alias commands for user (custom)
     if [ $keName = "Linux" ]; then
-        funcAddFile "\n# ALIAS COMMAND"
-        funcAddFile "alias iptables='sudo iptables'    # legacy of nefirewall management tool"
-        # source /etc/os-release
-        # if [ "$ID" = "ubuntu" ]; then
-        #     funcAddFile "alias ufw='sudo ufw'               # firewall management tool: ufw (uncomplicated firewall)"
-        #     funcAddFile "alias apt='sudo apt'               # for debian-based family"
-        #     funcAddFile "alias apt-get='sudo apt-get'       # legacy of debian-based family"
-        # elif [ "$ID" = "debian" ]; then
-        #     funcAddFile "alias nft='sudo nft'               # firewall management tool: nftables (netfilter table)"
-        #     funcAddFile "alias apt='sudo apt'               # for debian-based family"
-        #     funcAddFile "alias apt-get='sudo apt-get'       # legacy of debian-based family"
-        # elif [ "$ID" = "fedora" ] || [ "$ID" = "centos" ] || [ "$ID" = "rhel" ]; then
-        #     funcAddFile "alias firewall='sudo firewall-cmd' # firewall management tool: firewall"
-        #     funcAddFile "alias dnf='sudo dnf'               # for redhat-based family"
-        #     funcAddFile "alias yum='sudo yum'               # legacy of redhat-based family"
-        # elif [ "$ID" = "arch" ]; then
-        #     funcAddFile "alias pacman='sudo pacman'         # for arch-based family"
-        # elif [ "$ID" = "opensuse" ]; then
-        #     funcAddFile "alias zypper='sudo zypper'         # for suse-based family"
-        # fi
-    fi
-
-    # Disabed funtional/alias command part
-    if [ $keName = "Linux" ]; then
-        funcAddFile "\n# OPTIONAL COMMAND"
-        # source /etc/os-release
-        # if [ "$ID" = "ubuntu" ] || [ "$ID" = "debian" ] || [ "$ID" = "fedora" ] || [ "$ID" = "centos" ] || [ "$ID" = "rhel" ] || [ "$ID" = "arch" ] || [ "$ID" = "opensuse" ]; then
-            funcAddFile "#rmdir () { command rmdir -v \"\$@\" ; } "
-            funcAddFile "#mkdir () { command mkdir -v \"\$@\" ; } "
-            funcAddFile "#chmod () { command chmod --preserve-root \"\$@\" ; }"
-            funcAddFile "#chown () { command chown --preserve-root \"\$@\" ; }"
-            funcAddFile "#chgrp () { command chgrp --preserve-root \"\$@\" ; }"
-            funcAddFile "#vi () { command vim \"\$@\" ; }"
-            funcAddFile "#emcs () { emacs \"\$@\" ; }"
-            funcAddFile "#semcs () { sudo emacs \"\$@\" ; }"
-            funcAddFile "#semacs () { sudo emacs \"\$@\" ; }"
-            funcAddFile "#dfh () { df -h \"\$@\" ; }"
-            funcAddFile "#duh () { du -h \"\$@\" ; }"
-            funcAddFile "#alias vi='vim'"
-            funcAddFile "#alias top='htop'"
-            funcAddFile "#alias wget='wget -c'"
-            funcAddFile "#alias curl='curl -w \"\\\n\"'"
-            funcAddFile "#alias da='date'"
-            funcAddFile "#alias ca='cal'"
-            funcAddFile "#alias c='clear'"
-            funcAddFile "#alias f='finger'"
-            funcAddFile "#alias j='jobs -l'"
-            funcAddFile "#alias bc='bc -l'"
-        # else
-            funcAddFile "#alias nft='sudo nft'              # firewall management tool: nftables (netfilter table)"
-            funcAddFile "#alias ufw='sudo ufw'              # firewall management tool: ufw (uncomplicated firewall)"
-            funcAddFile "#alias firewall='sudo firewall-cmd'# firewall management tool: firewall"
-            funcAddFile "#alias apt='sudo apt'              # for debian-based family"
-            funcAddFile "#alias apt-get='sudo apt-get'      # legacy of debian-based family"
-            funcAddFile "#alias dnf='sudo dnf'              # for redhat-based family"
-            funcAddFile "#alias yum='sudo yum'              # legacy of redhat-based family"
-            funcAddFile "#alias pacman='sudo pacman'        # for arch-based family"
-            funcAddFile "#alias zypper='sudo zypper'        # for suse-based family"
-        # fi
+        funcAddFile "\n# USER COMMANDS"
+        funcAddFile "#alias top='htop'"
+        funcAddFile "#alias wget='wget -c'"
+        funcAddFile "#alias curl='curl -w \"\\\n\"'"
+        funcAddFile "#alias ca='cal'"
+        funcAddFile "#alias da='date'"
+        funcAddFile "#alias c='clear'"
+        funcAddFile "#alias f='finger'"
+        funcAddFile "#alias j='jobs -l'"
+        funcAddFile "#alias bc='bc -l'"
     fi
 }
-
 
 # MAIN
 funcTitle ""
 
-if [[ $1 = 1 ]]; then
-    osCode=1
-elif [[ $1 = 2 ]]; then
-    osCode=2
-    if [[ $2 = 1 ]]; then
-        luCode=1
-    elif [[ $2 = 2 ]]; then
-        luCode=2
-    elif [[ $2 = 3 ]]; then
-        luCode=3
-    elif [[ $2 = 4 ]]; then
-        luCode=4
-    elif [[ $2 = 5 ]]; then
-        luCode=5
-    elif [[ $2 = 6 ]]; then
-        luCode=6
-    elif [[ $2 = 7 ]]; then
-        luCode=7
-    elif [[ $2 = 8 ]]; then
-        luCode=8
-    elif [[ $2 = 9 ]]; then
-        luCode=9
-    else
-        echo "$msgReadme"
-        read -p "- Build Linux type: " luCode
-    fi
-    if [[ $3 = 1 ]]; then
-        shCode=1
-    elif [[ $3 = 2 ]]; then
-        shCode=2
-    else
-        echo "$msgReadme"
-        read -p "- Build shell type: " shCode
-    fi
-elif [[ $1 = 3 ]]; then
-    osCode=3
-else
-    echo "$msgReadme"
-    read -p "- Build OS type: " osCode
+if [ $1 = 1 ]; then osCode=1
+elif [ $1 = 2 ]; then osCode=2
+    if [ $2 = 1 ]; then luCode=1
+    elif [ $2 = 2 ]; then luCode=2
+    elif [ $2 = 3 ]; then luCode=3
+    elif [ $2 = 4 ]; then luCode=4
+    elif [ $2 = 5 ]; then luCode=5
+    elif [ $2 = 6 ]; then luCode=6
+    elif [ $2 = 7 ]; then luCode=7
+    elif [ $2 = 8 ]; then luCode=8
+    elif [ $2 = 9 ]; then luCode=9
+    else echo "$msgReadme"; read -p "- Build Linux type: " luCode; fi
+    if [ $3 = 1 ]; then shCode=1
+    elif [ $3 = 2 ]; then shCode=2
+    else echo "$msgReadme"; read -p "- Build shell type: " shCode; fi
+elif [ $1 = 3 ]; then osCode=3
+else echo "$msgReadme"; read -p "- Build OS type: " osCode
 fi
 
 if [ $osCode = 1 ]; then
@@ -445,51 +437,28 @@ if [ $osCode = 1 ]; then
     tacshFilePath="\"$tacshDirPath/tac.sh\""
     iCloudPath="\"\$HOME/Library/Mobile Documents/com~apple~CloudDocs\""
     dropboxPath="\"\$HOME/Library/CloudStorage/Dropbox\""
-elif [ $osCode = 2 ]; then
-    keName="Linux"
-    if [ -z $luCode ]; then
-        read -p "- Build Linux type: " luCode
-    fi
-    if [ -z $shCode ]; then
-        read -p "- Build shell type: " shCode
-    fi
-    if [ $luCode = 1 ]; then
-        osName="Ubuntu"
-    elif [ $luCode = 2 ]; then
-        osName="Debain"
-    elif [ $luCode = 3 ]; then
-        osName="Fedora"
-    elif [ $luCode = 4 ]; then
-        osName="CentOS"
-    elif [ $luCode = 5 ]; then
-        osName="REHL"
-    elif [ $luCode = 6 ]; then
-        osName="SUSE"
-    elif [ $luCode = 7 ]; then
-        osName="Arch"
-    elif [ $luCode = 8 ]; then
-        osName="Kali"
-    elif [ $luCode = 9 ]; then
-        osName="Other Linux"
-    else
-        funcWrong
-    fi
-    if [ $luCode = 9 ]; then
-        taschGenDirPath="$tacshGenDirPath/Linux"
-    else
-        taschGenDirPath="$tacshGenDirPath/Linux/$osName"
-    fi
-    if [ $shCode = 1 ]; then
-        shName="zsh"
+elif [ $osCode = 2 ]; then keName="Linux"
+    if [ -z $luCode ]; then read -p "- Build Linux type: " luCode; fi
+    if [ -z $shCode ]; then read -p "- Build shell type: " shCode; fi
+    if [ $luCode = 1 ]; then osName="Ubuntu"
+    elif [ $luCode = 2 ]; then osName="Debain"
+    elif [ $luCode = 3 ]; then osName="Fedora"
+    elif [ $luCode = 4 ]; then osName="CentOS"
+    elif [ $luCode = 5 ]; then osName="REHL"
+    elif [ $luCode = 6 ]; then osName="SUSE"
+    elif [ $luCode = 7 ]; then osName="Arch"
+    elif [ $luCode = 8 ]; then osName="Kali"
+    elif [ $luCode = 9 ]; then osName="Other Linux"
+    else funcWrong; fi
+    if [ $luCode = 9 ]; then taschGenDirPath="$tacshGenDirPath/Linux"
+    else taschGenDirPath="$tacshGenDirPath/Linux/$osName"; fi
+    if [ $shCode = 1 ]; then shName="zsh"
         taschGenFilePath="$taschGenDirPath/tac.zsh"
         tacshFilePath="\"$tacshDirPath/tac.zsh\""
-    elif [ $shCode = 2 ]; then
-        shName="bash"
+    elif [ $shCode = 2 ]; then shName="bash"
         taschGenFilePath="$taschGenDirPath/tac.bash"
         tacshFilePath="$tacshDirPath/tac.bash"
-    else 
-        funcWrong
-    fi
+    else funcWrong; fi
     dropboxPath="\"\$HOME/Dropbox\""
 elif [ $osCode = 3 ]; then
     keName="MINGW64"
@@ -519,6 +488,5 @@ elif [ $shName = "bash" ]; then
 fi
 
 echo "- Selected Enironment: $osName($keName) & $shName"
-
 funcGen
 echo "- Generated tacsh file: $taschGenFilePath"
